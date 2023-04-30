922646870604822e649ab76b7aab971d6be6bb36
keigo-h <keigo.hirohara@hive-japan.com>
2023-05-01 06:51:09 +0900

    チェックボックステスト追加

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
     <div>
       <button
         onClick={() => setButtonColor(newButtonColor)}
         style={{ backgroundColor: buttonColor }}
+        disabled={disabled}
       >
         Change to {buttonColor === 'red' ? 'Blue' : 'Red'}
       </button>
-      <input type="checkbox" />
+      <input type="checkbox" onChange={(e) => setDisabled(e.target.checked)} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -26,3 +26,13 @@ test('initial conditions', () => {
   const checkbox = screen.getByRole('checkbox');
   expect(checkbox).not.toBeChecked();
 });
+
+test('checkbox disables button on first click and enables on second click', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox');
+  const button = screen.getByRole('button', { name: 'Change to Blue' });
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+});
